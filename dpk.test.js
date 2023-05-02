5fe7d8e28c96c1884b170bcc0c032211142405b8
Jean <jeanc16rlos@gmail.com>
2023-05-01 22:06:24 -0400

    ✅  Add tests to current functionality

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,46 @@
-const { deterministicPartitionKey } = require("./dpk");
+const { deterministicPartitionKey } = require('./dpk');
+const crypto = require('crypto');
 
-describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+jest.mock('crypto', () => ({
+  createHash: jest.fn().mockReturnThis(),
+  update: jest.fn().mockReturnThis(),
+  digest: jest.fn().mockReturnThis().mockReturnValue('mockedHash'),
+}));
+
+describe('deterministicPartitionKey', () => {
+  test('returns trivial partition key when no event is provided', () => {
+    expect(deterministicPartitionKey()).toBe('0');
+  });
+
+  test('returns hash of event when no partition key is provided', () => {
+    const event = { id: 1, type: 'test' };
+    const expectedHash = 'mockedHash';
+    expect(deterministicPartitionKey(event)).toBe(expectedHash);
+  });
+
+  test('returns given partition key when provided', () => {
+    const event = { id: 1, type: 'test', partitionKey: 'customKey' };
+    expect(deterministicPartitionKey(event)).toBe('customKey');
+  });
+
+  test('converts non-string partition key to string', () => {
+    const event = { id: 1, type: 'test', partitionKey: 123 };
+    expect(deterministicPartitionKey(event)).toBe('123');
+  });
+
+  test('returns hash of partition key when it exceeds max length', () => {
+    const longKey = 'a'.repeat(257);
+    const event = { id: 1, type: 'test', partitionKey: longKey };
+    const expectedHash = 'mockedHash';
+    expect(deterministicPartitionKey(event)).toBe(expectedHash);
+  });
+
+  test('ensures the hash function is called with the correct input', () => {
+    const event = { id: 1, type: 'test' };
+    const eventString = JSON.stringify(event);
+    deterministicPartitionKey(event);
+    expect(crypto.createHash).toHaveBeenCalledWith('sha3-512');
+    expect(crypto.createHash().update).toHaveBeenCalledWith(eventString);
+    expect(crypto.createHash().update().digest).toHaveBeenCalledWith('hex');
   });
 });
