4e0980800fd1f2cf7a926196f9d86d433e423527
Jean <jeanc16rlos@gmail.com>
2023-05-01 22:39:20 -0400

    ♻️ Refactor tests to be more huma readable

--- dpk.test.js
+++ dpk.test.js
@@ -1,52 +1,73 @@
-const { deterministicPartitionKey } = require('./dpk');
-const crypto = require('crypto');
+const { deterministicPartitionKey } = require("./dpk");
+const crypto = require("crypto");
 
 // Mock crypto
-jest.mock('crypto', () => ({
+jest.mock("crypto", () => ({
   createHash: jest.fn().mockReturnThis(),
   update: jest.fn().mockReturnThis(),
-  digest: jest.fn().mockReturnThis().mockReturnValue('mockedHash'),
+  digest: jest.fn().mockReturnThis().mockReturnValue("mockedHash"),
 }));
 
-describe('deterministicPartitionKey', () => {
-  
-  it('should be defined', () => {
-    expect(deterministicPartitionKey).toBeDefined();
-  });
-  
-  it('should return trivial partition key when no event is provided', () => {
-    expect(deterministicPartitionKey()).toBe('0');
+describe("deterministicPartitionKey", () => {
+  describe("Given a deterministicPartitionKey function", () => {
+    it("should be defined", () => {
+      expect(deterministicPartitionKey).toBeDefined();
+    });
   });
 
-  it('should return hash of event when no partition key is provided', () => {
-    const event = { id: 1, type: 'test' };
-    const expectedHash = 'mockedHash';
-    expect(deterministicPartitionKey(event)).toBe(expectedHash);
+  describe("Given an event", () => {
+    describe("Given an empty event object", () => {
+      it("should return hash of empty event object", () => {
+        const event = {};
+        const expectedHash = "mockedHash";
+        expect(deterministicPartitionKey(event)).toBe(expectedHash);
+      });
+    });
+    describe("Given an event with no partition key", () => {
+      it("should return hash of event", () => {
+        const event = { id: 1, type: "test" };
+        const expectedHash = "mockedHash";
+        expect(deterministicPartitionKey(event)).toBe(expectedHash);
+      });
+    });
+    describe("Given an event with a partition key", () => {
+      describe("Given a partition key that is a string", () => {
+        it("should return given partition key", () => {
+          const event = { id: 1, type: "test", partitionKey: "customKey" };
+          expect(deterministicPartitionKey(event)).toBe("customKey");
+        });
+        it("should make sure the hash function is called with the correct input", () => {
+          const event = { id: 1, type: "test" };
+          const eventString = JSON.stringify(event);
+          deterministicPartitionKey(event);
+          expect(crypto.createHash).toHaveBeenCalledWith("sha3-512");
+          expect(crypto.createHash().update).toHaveBeenCalledWith(eventString);
+          expect(crypto.createHash().update().digest).toHaveBeenCalledWith(
+            "hex"
+          );
+        });
+      });
+
+      describe("Given a partition key that is a number", () => {
+        it("should convert non-string partition key to string", () => {
+          const event = { id: 1, type: "test", partitionKey: 123 };
+          expect(deterministicPartitionKey(event)).toBe("123");
+        });
+      });
+      describe("Given a partition key that exceeds max length", () => {
+        it("should return hash of partition key", () => {
+          const longKey = "a".repeat(257);
+          const event = { id: 1, type: "test", partitionKey: longKey };
+          const expectedHash = "mockedHash";
+          expect(deterministicPartitionKey(event)).toBe(expectedHash);
+        });
+      });
+    });
   });
 
-  it('should return given partition key when provided', () => {
-    const event = { id: 1, type: 'test', partitionKey: 'customKey' };
-    expect(deterministicPartitionKey(event)).toBe('customKey');
-  });
-
-  it('should convert non-string partition key to string', () => {
-    const event = { id: 1, type: 'test', partitionKey: 123 };
-    expect(deterministicPartitionKey(event)).toBe('123');
-  });
-
-  it('should return hash of partition key when it exceeds max length', () => {
-    const longKey = 'a'.repeat(257);
-    const event = { id: 1, type: 'test', partitionKey: longKey };
-    const expectedHash = 'mockedHash';
-    expect(deterministicPartitionKey(event)).toBe(expectedHash);
-  });
-
-  it('should ensures the hash function is called with the correct input', () => {
-    const event = { id: 1, type: 'test' };
-    const eventString = JSON.stringify(event);
-    deterministicPartitionKey(event);
-    expect(crypto.createHash).toHaveBeenCalledWith('sha3-512');
-    expect(crypto.createHash().update).toHaveBeenCalledWith(eventString);
-    expect(crypto.createHash().update().digest).toHaveBeenCalledWith('hex');
+  describe("Not Given an event", () => {
+    it("should return trivial partition key", () => {
+      expect(deterministicPartitionKey()).toBe("0");
+    });
   });
 });
