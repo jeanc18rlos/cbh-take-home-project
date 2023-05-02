1f04784d51b1d92c4fa78ce487252e10926ae383
Jean <jeanc16rlos@gmail.com>
2023-05-01 22:20:32 -0400

    ♻️ Broke down logic into smaller chunks

--- dpk.js
+++ dpk.js
@@ -1,28 +1,23 @@
-const crypto = require("crypto");
+const crypto = require('crypto');
+
+const TRIVIAL_PARTITION_KEY = '0';
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+const hash = (data) => crypto.createHash('sha3-512').update(data).digest('hex');
+
+const getPartitionKeyFromEvent = (event) => {
+  if (!event) return TRIVIAL_PARTITION_KEY;
+  if (event.partitionKey) return event.partitionKey;
+  return hash(JSON.stringify(event));
+};
+
+const enforceStringLength = (key) => {
+  if (key.length > MAX_PARTITION_KEY_LENGTH) return hash(key);
+  return key;
+};
 
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  let partitionKey = getPartitionKeyFromEvent(event);
+  partitionKey = enforceStringLength(String(partitionKey));
+  return partitionKey;
 };
